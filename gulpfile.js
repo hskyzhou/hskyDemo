e5db0b460e3215b94648fbbec819f8998700bc98
hsky <wen.zhou@bioon.com>
2017-02-07 10:40:11 +0800

    完成修改

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ require('laravel-elixir-vue');
 
 var elixirTypscript = require('elixir-typescript');
 require('laravel-elixir-livereload');
-var BrowserSync = require('laravel-elixir-browsersync2');
 
 var BrowserSync = require('laravel-elixir-browsersync2');
 
@@ -26,11 +25,11 @@ elixir(mix => {
 
     // mix.typescript('main.ts', 'public/');
 
-    mix.copy('node_modules/@angular', 'public/@angular');
-    mix.copy('node_modules/core-js', 'public/core-js');
-    mix.copy('node_modules/zone.js', 'public/zone.js');
-    mix.copy('node_modules/systemjs', 'public/systemjs');
-    mix.copy('node_modules/rxjs', 'public/rxjs');
+   //  mix.copy('node_modules/@angular', 'public/@angular');
+   //  mix.copy('node_modules/core-js', 'public/core-js');
+   //  mix.copy('node_modules/zone.js', 'public/zone.js');
+   //  mix.copy('node_modules/systemjs', 'public/systemjs');
+   //  mix.copy('node_modules/rxjs', 'public/rxjs');
 
     mix.typescript('app/*.ts', 'public/app', null, {
 	    "target": "es5",
@@ -44,35 +43,37 @@ elixir(mix => {
 	    "suppressImplicitAnyIndexErrors": true
 	  });
 
-    mix.typescript('main.ts', 'public/main.js', null, {
-	    "target": "es5",
-	    "module": "commonjs",
-	    "moduleResolution": "node",
-	    "sourceMap": true,
-	    "emitDecoratorMetadata": true,
-	    "experimentalDecorators": true,
-	    "lib": [ "es2015", "dom" ],
-	    "noImplicitAny": true,
-	    "suppressImplicitAnyIndexErrors": true
-	  });
+   //  mix.typescript('main.ts', 'public/main.js', null, {
+	  //   "target": "es5",
+	  //   "module": "commonjs",
+	  //   "moduleResolution": "node",
+	  //   "sourceMap": true,
+	  //   "emitDecoratorMetadata": true,
+	  //   "experimentalDecorators": true,
+	  //   "lib": [ "es2015", "dom" ],
+	  //   "noImplicitAny": true,
+	  //   "suppressImplicitAnyIndexErrors": true
+	  // });
 
     // BrowserSync.init({
     // 	port : 8080,
     // 	proxy : 'hsky.demo.me'
     // });
     // mix.BrowserSync();
+
+    mix.livereload();
     BrowserSync.init();
     mix.BrowserSync(
     {
+    		localOnly: true,
         proxy : "hsky.demo.me",
-    	port : 8080,
+    		port : 10000,
         logPrefix       : "Laravel Eixir BrowserSync",
         logConnections  : false,
         reloadOnRestart : false,
         notify          : false,
         host : "hsky.demo.me",
         open: "external",
-        files: ["public/app/*/*.js"]
-
+        // files: ["public/app/*/*.js"]
     });
 });
